2461064f6ee779b0c4e19013b3d8419c4ef9b71a
thanhphamtrung <thanhpt22emu@gmail.com>
2022-12-10 10:40:31 +0700

    Update problem_1.py

--- problem_1.py
+++ problem_1.py
@@ -27,9 +27,9 @@ def binary_search(target, start, end):
          return binary_search(target, start_index, mid - 1)
 
 
-# print("Pass" if (3 == sqrt(9)) else "Fail")
-# print("Pass" if (0 == sqrt(0)) else "Fail")
-# print("Pass" if (4 == sqrt(16)) else "Fail")
-# print("Pass" if (1 == sqrt(1)) else "Fail")
-# print("Pass" if (5 == sqrt(27)) else "Fail")
+print("Pass" if (3 == sqrt(9)) else "Fail")
+print("Pass" if (0 == sqrt(0)) else "Fail")
+print("Pass" if (4 == sqrt(16)) else "Fail")
+print("Pass" if (1 == sqrt(1)) else "Fail")
+print("Pass" if (5 == sqrt(27)) else "Fail")
 print("Pass" if (65071685350 == sqrt(4234324234324234234234)) else "Fail")
